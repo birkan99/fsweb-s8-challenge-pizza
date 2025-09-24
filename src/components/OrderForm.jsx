0c031171a3356881468f85e54d8af9f522e80d65
Birkan Sarıbacak <birkan35@hotmail.com.tr>
2025-09-24 12:08:34 +0300

    siparis notu duzeltildi

--- src/components/OrderForm.jsx
+++ src/components/OrderForm.jsx
@@ -62,6 +62,7 @@ export default function OrderForm() {
             extras: extras,
             total: total,
             quantity: quantity,
+            note: note,
           },
         },
       });
--- src/components/SuccessPage.jsx
+++ src/components/SuccessPage.jsx
@@ -4,7 +4,7 @@ import Footer from "./Footer";
 
 export default function SuccessPage() {
   const location = useLocation();
-  const order = location.state?.order; 
+  const order = location.state?.order;
 
   if (!order) {
     return (
@@ -14,7 +14,7 @@ export default function SuccessPage() {
     );
   }
 
-  const { title, size, dough, extras, total } = order;
+  const { title, size, dough, extras, total, note } = order;
 
   return (
     <div className="flex flex-col min-h-screen bg-red-600 text-white">
@@ -24,7 +24,7 @@ export default function SuccessPage() {
           lezzetin yolda
         </h2>
         <h1 className="text-5xl sm:text-6xl md:text-7xl font-[roboto-condensed] tracking-wider border-b-2 pb-6 mb-10">
-          SİPARİŞ ALINDI  
+          SİPARİŞ ALINDI
         </h1>
         <div className="flex flex-col gap-4 text-white text-lg sm:text-xl">
           <p className="font-[semibold] text-3xl  text-center">{title}</p>
@@ -36,8 +36,15 @@ export default function SuccessPage() {
           </p>
           {extras.length > 0 && (
             <p>
-              <span className="font-[semibold]">Ek Malzemeler:</span> {extras.join(", ")}
+              <span className="font-[semibold]">Ek Malzemeler:</span>{" "}
+              {extras.join(", ")}
             </p>
+          )}{" "}
+          {note && ( // Sadece not varsa göster
+            <div className="mt-4">
+              <p className="font-semibold text-lg">Sipariş Notu:</p>
+              <p className="text-white text-md italic">{note}</p>{" "}
+            </div>
           )}
         </div>
         <div className=" text-white border-2 p-8 rounded-lg shadow-md mt-10 w-full max-w-sm">
